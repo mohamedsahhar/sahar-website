fadaf2b9061dfb57ed3bbe4bbecb14066b138dd7
Sa7ar Quick Care <mohamedsahhar@gmail.com>
2026-04-20 11:27:25 +0200

    Upgrade admin middleware role protection

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,19 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth({
-  pages: {
-    signIn: "/admin/login",
-  },
-});
+export default withAuth(
+  function middleware(req) {},
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        return token?.role === "SUPER_ADMIN";
+      },
+    },
+
+    pages: {
+      signIn: "/admin/login",
+    },
+  }
+);
 
 export const config = {
   matcher: [
